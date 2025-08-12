137696e405b4212403da61e70690121fbc0b47db
youness bachar khir <81125054+younessbachar@users.noreply.github.com>
2025-08-12 20:45:20 +0100

    first commit

--- week_1/day_1/daily_challengs/daily_challenge.py
+++ week_1/day_1/daily_challengs/daily_challenge.py
@@ -1,4 +1,25 @@
 # ------ Challenge 1
+number = int(input("Enter a number: "))
+lenght = int(input("Enter a lenght: "))
+number2 = 0
+list = []
+    
+for i in range(lenght) :
+    number2 += number
+    list.append(number2)
+    
+print(list)
 
 # ------ Challenge 2
+
+word = input("Enter a word: ")
+
+if word:
+   result = word[0]
+   for char in word[1:len(word)]:
+      if char != result[-1]:
+        result += char
+else:
+  result = ""
+print("empty input", result)
 
