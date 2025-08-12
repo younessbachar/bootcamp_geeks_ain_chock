3cdf1d38bee9d5e87490d20adfd65a289a17bdf2
youness bachar khir <81125054+younessbachar@users.noreply.github.com>
2025-08-12 10:30:30 +0100

    first commit

---
+++ week_1/day_1/exercises/exercises_xp_gold.py
@@ -0,0 +1,25 @@
+# ------ Exercise 1
+
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+
+# ------ Exercise 2
+
+result = (99 ** 3) * 8
+
+print(result)
+
+
+# ------ Exercise 3
+
+# ------ Exercise 4
+
+# ------ Exercise 5
+
+# ------ Exercise 6
+
+# ------ Exercise 7
+
+# ------ Exercise 8
