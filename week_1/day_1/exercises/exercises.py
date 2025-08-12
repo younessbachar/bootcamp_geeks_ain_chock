51143847cb2e84ab22d16ea4d090606612acba70
youness bachar khir <81125054+younessbachar@users.noreply.github.com>
2025-08-12 12:17:15 +0100

    first day

--- week_1/day_1/exercises/exercises.py
+++ week_1/day_1/exercises/exercises.py
@@ -1,15 +1,81 @@
 # ------ Exercise 1
 
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+
 # ------ Exercise 2
+
+result = (99 ** 3) * 8
+
+print(result)
+
 
 # ------ Exercise 3
 
+inp = input("Enter your name: ")
+
+print("Hello, "+ inp)
+
 # ------ Exercise 4
+
+height = int(input("Enter your height in metres : "))
+
+if height > 145 :
+  print("you are tall enough to ride")
+else:
+  print("you are not tall enough to ride")
+
 
 # ------ Exercise 5
 
+My_fav_numbers = {6,45,30,15,7,4}
+Friend_fav_numbers = {8,0,45,67,2,1}
+
+My_fav_numbers.add(3)
+My_fav_numbers.add(9)
+My_fav_numbers.remove(9)
+print(My_fav_numbers)
+
+
+conc = My_fav_numbers.union(Friend_fav_numbers)
+print(conc)
+
+
 # ------ Exercise 6
+
+#(yes because it is a inmutable)
 
 # ------ Exercise 7
 
+basket = ["Banana", "Apples", "Oranges", "Blueberries"]
+basket.remove("Banana")
+basket.remove("Blueberries")
+basket.append("kiwi")
+basket.append("Apples")
+
+x = basket.count("Apples")
+
+basket.clear()
+
+print(basket)
+
 # ------ Exercise 8
+
+sandwich_orders = ["Tuna sandwich", "Pastrami sandwich", "Avocado sandwich", "Pastrami sandwich", "Egg sandwich", "Chicken sandwich", "Pastrami sandwich"]
+
+while "Pastrami sandwich" in sandwich_orders:
+    sandwich_orders.remove("Pastrami sandwich")
+
+fineshed_sandwiches = []
+
+while sandwich_orders:
+    current_sandwich = sandwich_orders.pop()
+    fineshed_sandwiches.append(current_sandwich)
+
+      
+
+print(fineshed_sandwiches)
+
+
--- week_1/day_1/exercises/exercises_xp_gold.py
+++ week_1/day_1/exercises/exercises_xp_gold.py
@@ -1,25 +1,69 @@
+import random
 # ------ Exercise 1
+spring = [3,4,5]
+summer = [6,7,8]
+autumn = [9,10,11]
+winter = [12,1,2]
 
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
+
+inp = int(input("Invalid input. Please enter a number between 1 and 12: "))
+
+while inp < 1 or inp > 12:
+    inp = int(input("Invalid input. Please enter a number between 1 and 12: "))
+if inp in spring:
+  print("Spring")
+elif inp in summer:
+  print("Summer")
+elif inp in autumn:
+  print("Autumn")
+elif inp in winter:
+  print("Winter")
 
 # ------ Exercise 2
 
-result = (99 ** 3) * 8
+for i in range(1, 21):
+  print(i)
 
-print(result)
-
+for i in range(1, 21):
+  if i % 2 == 0:
+    print(i)
 
 # ------ Exercise 3
 
+name = input("Please enter your name: ")
+while name != "youness" :
+    name = input("Please enter your name: ")
+
+
 # ------ Exercise 4
 
+names = ['Samus', 'Cortana', 'V', 'Link', 'Mario', 'Cortana', 'Samus']
+input_name = input("Please enter a name: ")
+for name in names:
+    if input_name == name:
+        print(f"The index of {name} is {names.index(name)}")
+        break
 # ------ Exercise 5
 
+numbers = []
+
+for i in range(0 , 3):
+   currentnum = int(input("Please enter a number: "))
+   numbers.append(currentnum)
+
+print("The largest number is: ", max(numbers))
+
 # ------ Exercise 6
+wins = 0
+losses = 0
 
-# ------ Exercise 7
-
-# ------ Exercise 8
+usernum = int(input("Enter a number: "))
+num = random.randint(1, 10)
+while True:
+    usernum = int(input("Enter a number: "))
+    if usernum == num:
+     print("Winner")
+     wins += 1
+    else:
+     print("etter luck next time")
+     losses += 1
