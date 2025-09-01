14542d4eab33e9b7c28065e28b52460b4326c88e
youness bachar khir <81125054+younessbachar@users.noreply.github.com>
2025-09-01 11:47:09 +0100

    week2 day1 exercisexp

---
+++ week_2/Day1/exercisexp.sql
@@ -0,0 +1,21 @@
+elect * from students;
+
+select first_name, last_name from students;
+
+select first_name , last_name from students where id = 2;
+
+select first_name , last_name from students where last_name = 'Benichou' and first_name = 'Marc';
+
+select first_name , last_name from students where last_name = 'Benichou' or first_name = 'Marc';
+
+select first_name , last_name from students where first_name like '%a%';
+
+select first_name , last_name from students where first_name like 'a%';
+
+select first_name , last_name from students where first_name like '%a';
+
+select first_name , last_name from students where last_name like '%a_';
+
+select first_name , last_name from students where id in (1,3);
+
+select first_name , last_name from students where birth_date >= '1/01/2000'
