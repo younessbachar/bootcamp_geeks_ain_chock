97c980530c27cc4eaa2fc77e55fa8804c21674d6
Youness Bachar-khir <81125054+younessbachar@users.noreply.github.com>
2025-09-17 14:06:26 +0100

    Update exercisexp.sql

--- week_2/Day1/exercises/exercisexp.sql
+++ week_2/Day1/exercises/exercisexp.sql
@@ -1,4 +1,23 @@
-elect * from students;
+CREATE DATABASE IF NOT EXISTS students_db;
+USE students_db;
+
+CREATE TABLE students (
+  id INT PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  birth_date DATE
+);
+
+INSERT INTO students (id, first_name, last_name, birth_date) VALUES
+(1, 'Marc',  'Benichou', '1994-06-17'),
+(2, 'Amina', 'Benichou', '2002-03-08'),
+(3, 'Sara',  'Elhadad',  '1999-12-05'),
+(4, 'Adam',  'Khan',     '2000-12-01'),
+(5, 'Laila', 'Hanae',    '1998-11-25'),
+(6, 'Younes','Bchar',    '1997-09-10');
+
+
+select * from students;
 
 select first_name, last_name from students;
 
